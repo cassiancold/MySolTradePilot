98178f71b6c9d0fdcb497f87c111b47569722165
cassiancold <ojochoco51@gmail.com>
2026-04-01 00:04:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -521,7 +521,7 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(CallbackQueryHandler(button_handler))
-    app.add_handler(MessageHandler(filters.TEXT & \~filters.COMMAND, handle_text))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
 
     print("🚀 SolTradePilotBot with Custom Deposit is running!")
     app.run_polling()
